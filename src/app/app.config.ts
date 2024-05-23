66f8aa682211ac5130ca056e2fe1aefd8ebee300
BryanAv7 <bavilag@est.ups.edu.ec>
2024-05-23 00:18:24 -0500

    probando

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"estudio1-2eac0","appId":"1:619799004073:web:58facf89b51138bd4bbab8","storageBucket":"estudio1-2eac0.appspot.com","apiKey":"AIzaSyDLKRlCv_-ywsq-kxJTrw3S7tydrFhQ6Vc","authDomain":"estudio1-2eac0.firebaseapp.com","messagingSenderId":"619799004073","measurementId":"G-VY8G88E5CP"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
